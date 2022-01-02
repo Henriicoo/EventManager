bfc111980eb266378a36da7c691f5b0a18a8e95b
HenriqueNapimo1 <henriquenapimo1@gmail.com>
2022-01-02 20:05:26 -0300

    Custom join message if there is any event happening

--- src/main/java/com/henriquenapimo1/eventmanager/listeners/EventListener.java
+++ src/main/java/com/henriquenapimo1/eventmanager/listeners/EventListener.java
@@ -1,7 +1,9 @@
 package com.henriquenapimo1.eventmanager.listeners;
 
 import com.henriquenapimo1.eventmanager.Main;
-import com.henriquenapimo1.eventmanager.utils.objetos.Evento;
+import com.henriquenapimo1.eventmanager.utils.Utils;
+import com.henriquenapimo1.eventmanager.utils.objetos.*;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -9,6 +11,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class EventListener implements Listener {
@@ -18,6 +21,37 @@ public class EventListener implements Listener {
         Evento e = Main.getMain().evento;
         if(e != null && e.getPlayers().contains(event.getPlayer())) {
             e.removePlayer(event.getPlayer(),false);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        if(event.getPlayer().hasPermission("eventmanager.staff")) return;
+
+        // se tem evento
+        Evento e = Main.getMain().evento;
+        if(e != null && event.getPlayer().hasPermission("eventmanager.entrar")) {
+            event.getPlayer().spigot().sendMessage(Utils.getAnuncio(e).create());
+        }
+        // se tem quiz
+        Quiz q = Main.getMain().quiz;
+        if(q != null && event.getPlayer().hasPermission("eventmanager.quiz.responder")) {
+            event.getPlayer().spigot().sendMessage(Utils.getAnuncio(q).create());
+        }
+        // se tem vouf
+        Vouf v = Main.getMain().vouf;
+        if(v != null && event.getPlayer().hasPermission("eventmanager.vouf.responder")) {
+            event.getPlayer().spigot().sendMessage(Utils.getAnuncio(v).create());
+        }
+        //se tem bolão
+        Bolao b = Main.getMain().bolao;
+        if(b != null && event.getPlayer().hasPermission("eventmanager.bolao.apostar")) {
+            event.getPlayer().spigot().sendMessage(Utils.getAnuncio(b).create());
+        }
+        // se tem loteria
+        Loteria l = Main.getMain().loteria;
+        if(l != null && event.getPlayer().hasPermission("eventmanager.loteria.apostar")) {
+            event.getPlayer().spigot().sendMessage(Utils.getAnuncio(l).create());
         }
     }
 
@@ -54,9 +88,11 @@ public class EventListener implements Listener {
         if(e == null) return;
 
         if(e.getPlayers().contains(event.getEntity())) {
+            event.getDrops().clear();
             if(e.getFlags().getRespawn()) {
-                e.addDeadPlayer(event.getEntity());
+                Bukkit.getScheduler().runTaskLater(Main.getMain(), () -> e.addDeadPlayer(event.getEntity()),20);
             } else {
+                event.getEntity().sendMessage("§7Você morreu, por isso, foi removido do evento!");
                 e.removePlayer(event.getEntity(),false);
             }
         }
